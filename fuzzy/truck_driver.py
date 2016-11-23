337771d04f7923108bd9a7db859e9ece78c7742e
Gustavo Zambonin <gzmbnn@gmail.com>
2016-11-23 17:07:32 -0200

    fuzzy: updated rules to reflect a more desired driver behaviour

--- fuzzy/truck_driver.py
+++ fuzzy/truck_driver.py
@@ -21,7 +21,7 @@ import socket
 import matplotlib.pyplot as plt
 
 from math import floor
-from numpy import arange, dstack, fmin
+from numpy import arange, fmin, fmax
 from skfuzzy import defuzz, interp_membership, trimf
 from sys import argv
 
@@ -44,7 +44,7 @@ def _min(_list):
 
 def normalize_angle(angle):
     """
-    Normalizes from [0, 360] to [-30, 30].
+    Shifts the truck to the right.
 
     Args:
         angle:  the number to be normalized.
@@ -52,19 +52,13 @@ def normalize_angle(angle):
     Returns:
         The normalized number.
     """
-    rotation = floor(((angle + 90) % 360.0) - 180.0)
 
-    if (rotation > 30):
-        rotation = 30
-    if (rotation < -30):
-        rotation = -30
-
-    return rotation
+    return floor(((angle + 90) % 360.0) - 180.0)
 
 
 def plot_fuzzy_sets():
     """Shows the fuzzy sets constructed below in pretty colors."""
-    fig, (ax0, ax1, ax2, ax3) = plt.subplots(nrows=4, figsize=(8, 9))
+    fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, figsize=(8, 9))
 
     ax0.plot(x_dist, x_lo, 'b', linewidth=1.5, label='Left')
     ax0.plot(x_dist, x_md, 'g', linewidth=1.5, label='Center')
@@ -72,28 +66,24 @@ def plot_fuzzy_sets():
     ax0.set_title('X Distance')
     ax0.legend()
 
-    ax1.plot(y_dist, y_lo, 'b', linewidth=1.5, label='Far')
-    ax1.plot(y_dist, y_hi, 'r', linewidth=1.5, label='Near')
-    ax1.set_title('Y Distance')
+    ax1.plot(angle_range, a_strg_left,  'b', linewidth=1.5, label='Str. left')
+    ax1.plot(angle_range, a_weak_left,  'g', linewidth=1.5, label='Left')
+    ax1.plot(angle_range, a_straight,   'r', linewidth=1.5, label='Straight')
+    ax1.plot(angle_range, a_weak_right, 'k', linewidth=1.5, label='Right')
+    ax1.plot(angle_range, a_strg_right, 'y', linewidth=1.5, label='Str. right')
+    ax1.set_title('Rotation Power')
+    ax1.set_xlim([-35, 35])
     ax1.legend()
 
-    ax2.plot(angle_range, a_strg_left,  'b', linewidth=1.5, label='Str. left')
-    ax2.plot(angle_range, a_weak_left,  'g', linewidth=1.5, label='Left')
-    ax2.plot(angle_range, a_straight,   'r', linewidth=1.5, label='Straight')
-    ax2.plot(angle_range, a_weak_right, 'k', linewidth=1.5, label='Right')
-    ax2.plot(angle_range, a_strg_right, 'y', linewidth=1.5, label='Str. right')
-    ax2.set_title('Rotation Power')
+    ax2.plot(intensity, steer_lvl01, 'g', linewidth=1.5, label='Lower')
+    ax2.plot(intensity, steer_lvl02, 'b', linewidth=1.5, label='Low')
+    ax2.plot(intensity, steer_lvl03, 'r', linewidth=1.5, label='Medium')
+    ax2.plot(intensity, steer_lvl05, 'k', linewidth=1.5, label='High')
+    ax2.plot(intensity, steer_lvl06, 'y', linewidth=1.5, label='Higher')
+    ax2.set_title('Turn Power')
     ax2.legend()
 
-    ax3.plot(intensity, steer_lvl01, 'g', linewidth=1.5, label='Lower')
-    ax3.plot(intensity, steer_lvl02, 'b', linewidth=1.5, label='Low')
-    ax3.plot(intensity, steer_lvl03, 'r', linewidth=1.5, label='Medium')
-    ax3.plot(intensity, steer_lvl05, 'k', linewidth=1.5, label='High')
-    ax3.plot(intensity, steer_lvl06, 'y', linewidth=1.5, label='Higher')
-    ax3.set_title('Turn Power')
-    ax3.legend()
-
-    for ax in (ax0, ax1, ax2, ax3):
+    for ax in (ax0, ax1, ax2):
         ax.spines['top'].set_visible(False)
         ax.spines['right'].set_visible(False)
         ax.get_xaxis().tick_bottom()
@@ -129,46 +119,53 @@ def drive_truck():
         x_lvl_md = interp_membership(x_dist, x_md, x)
         x_lvl_hi = interp_membership(x_dist, x_hi, x)
 
-        y_lvl_lo = interp_membership(y_dist, y_lo, y)
-        y_lvl_hi = interp_membership(y_dist, y_hi, y)
-
         strg_right = interp_membership(angle_range, a_strg_right, rot)
         weak_right = interp_membership(angle_range, a_weak_right, rot)
         straight = interp_membership(angle_range, a_straight, rot)
         weak_left = interp_membership(angle_range, a_weak_left, rot)
         strg_left = interp_membership(angle_range, a_strg_left, rot)
 
-        rules = [
-            [x_lvl_lo, y_lvl_lo, weak_left,  steer_lvl06],
-            [x_lvl_lo, y_lvl_lo, weak_right, steer_lvl06],
-            [x_lvl_lo, y_lvl_lo, straight,   steer_lvl06],
-            [x_lvl_md, y_lvl_lo, weak_left,  steer_lvl05],
-            [x_lvl_md, y_lvl_lo, weak_right, steer_lvl02],
-            [x_lvl_hi, y_lvl_lo, weak_left,  steer_lvl04],
-            [x_lvl_hi, y_lvl_lo, weak_right, steer_lvl01],
-            [x_lvl_hi, y_lvl_lo, straight,   steer_lvl01],
-            [x_lvl_lo, y_lvl_hi, weak_left,  steer_lvl05],
-            [x_lvl_lo, y_lvl_hi, weak_right, steer_lvl05],
-            [x_lvl_lo, y_lvl_hi, straight,   steer_lvl05],
-            [x_lvl_hi, y_lvl_hi, weak_left,  steer_lvl02],
-            [x_lvl_hi, y_lvl_hi, weak_right, steer_lvl02],
-            [x_lvl_hi, y_lvl_hi, straight,   steer_lvl02],
-            [x_lvl_lo,           strg_left,  steer_lvl06],
-            [x_lvl_lo,           strg_right, steer_lvl06],
-            [x_lvl_md,           weak_left,  steer_lvl05],
-            [x_lvl_md,           weak_right, steer_lvl02],
-            [x_lvl_md,           straight,   steer_lvl04],
-            [x_lvl_md,           strg_left,  steer_lvl06],
-            [x_lvl_md,           strg_right, steer_lvl01],
-            [x_lvl_hi,           strg_left,  steer_lvl02],
-            [x_lvl_hi,           strg_right, steer_lvl01],
+        rules_level_06 = [
+            [x_lvl_lo, strg_left,  steer_lvl06],
+            [x_lvl_lo, weak_left,  steer_lvl06],
+            [x_lvl_lo, straight,   steer_lvl06],
+            [x_lvl_lo, strg_right, steer_lvl06],
+            [x_lvl_md, strg_left,  steer_lvl06],
         ]
 
-        # stacks the minimized arrays into a single array and
-        # applies maximization with respect to the new axis
-        aggregated = dstack(map(_min, rules)).max(2)
-        steer_value = defuzz(intensity, aggregated, 'centroid')
+        rules_level_05 = [
+            [x_lvl_lo, weak_right, steer_lvl05],
+            [x_lvl_md, weak_left,  steer_lvl05],
+        ]
 
+        rules_level_04 = [
+            [x_lvl_lo, strg_right, steer_lvl04],
+            [x_lvl_md, straight,   steer_lvl04],
+            [x_lvl_hi, strg_left,  steer_lvl04],
+        ]
+
+        rules_level_02 = [
+            [x_lvl_md, weak_right, steer_lvl02],
+            [x_lvl_hi, weak_left,  steer_lvl02],
+        ]
+
+        rules_level_01 = [
+            [x_lvl_md, strg_right, steer_lvl01],
+            [x_lvl_hi, straight,   steer_lvl01],
+            [x_lvl_hi, weak_right, steer_lvl01],
+            [x_lvl_hi, strg_left,  steer_lvl01],
+            [x_lvl_hi, strg_right, steer_lvl01],
+        ]
+
+        aggregated = [
+            fmax.reduce(list(map(_min, rules_level_01))),
+            fmax.reduce(list(map(_min, rules_level_02))),
+            fmax.reduce(list(map(_min, rules_level_04))),
+            fmax.reduce(list(map(_min, rules_level_05))),
+            fmax.reduce(list(map(_min, rules_level_06))),
+        ]
+
+        steer_value = defuzz(intensity, fmax.reduce(aggregated), 'centroid')
         sckt.send((str(steer_value) + '\r\n').encode())
 
     sckt.close()
@@ -183,31 +180,27 @@ if __name__ == '__main__':
     # limitations from the server
     x_dist = arange(0, 1.1, 0.1)
     y_dist = arange(0, 1.1, 0.1)
-    angle_range = arange(-31, 31, 1)
+    angle_range = arange(-361, 361, 1)
 
     # the step value for this range can be customized
     intensity = arange(-1, 1, 0.25)
 
     # position along the X axis
-    x_lo = trimf(x_dist, [0.0, 0.0, 0.3])
-    x_md = trimf(x_dist, [0.2, 0.5, 0.7])
-    x_hi = trimf(x_dist, [0.6, 1.0, 1.0])
-
-    # position along the Y axis
-    y_lo = trimf(y_dist, [0.0, 0.0, 0.5])
-    y_hi = trimf(y_dist, [0.2, 1.0, 1.0])
+    x_lo = trimf(x_dist, [0.0, 0.0, 0.5])
+    x_md = trimf(x_dist, [0.2, 0.5, 0.8])
+    x_hi = trimf(x_dist, [0.5, 1.0, 1.0])
 
     # angle of truck
-    a_strg_right = trimf(angle_range, [10, 30, 30])
-    a_weak_right = trimf(angle_range, [0, 10, 20])
-    a_straight = trimf(angle_range, [-4, 0, 4])
-    a_weak_left = trimf(angle_range, [-20, -10, 0])
-    a_strg_left = trimf(angle_range, [-30, -30, -10])
+    a_strg_right = trimf(angle_range, [10, 360, 360])
+    a_weak_right = trimf(angle_range, [0, 15, 30])
+    a_straight = trimf(angle_range, [-5, 0, 5])
+    a_weak_left = trimf(angle_range, [-30, -15, 0])
+    a_strg_left = trimf(angle_range, [-360, -360, -10])
 
     # level of steering (lower level means steering to the left)
     steer_lvl01 = trimf(intensity, [-1.0, -1.0, -0.5])
     steer_lvl02 = trimf(intensity, [-0.8, -0.3, 0.0])
-    steer_lvl03 = trimf(intensity, [-0.5, 0.0, 0.5])
+    steer_lvl03 = trimf(intensity, [-0.2, 0.0, 0.2])
     steer_lvl04 = trimf(intensity, [-0.1, 0.0, 0.1])
     steer_lvl05 = trimf(intensity, [0.0, 0.3, 0.8])
     steer_lvl06 = trimf(intensity, [0.5, 1.0, 1.0])
